0ef574e30ce7bebdf8558a94bee2cedd69ead9b4
Rhian Felipe Silva de Lara <62002116+RhianFelipe@users.noreply.github.com>
2023-02-06 16:04:58 -0300

    Update script.js

--- script.js
+++ script.js
@@ -15,7 +15,7 @@ document.addEventListener("keydown", keyPush )
     var quantidadePecas = 20 //quantidade de peças no tabuleiro
     var ax = ay = 15 //posição da maça 
     var trail = [] 
-    tail = 10 
+    tail = 100 
   
     function  game() {
       posicaoNasceCobraX += vx
